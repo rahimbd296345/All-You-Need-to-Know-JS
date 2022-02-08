6ef837c093ef5275f1bf04790a7e14149055a587
Abdur Rahim <www.shatotacorporation@gmail.com>
2022-02-08 09:13:02 +0600

    Series number for loops

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-var Series
 
+var Series
 for (var i = 1; i <= 10; i++){
     series = i + ' = '
-    for (var j = 1; j < 10; j++) {
-        series += i * j + ' '
+    for (var j = 1; j < 10; j++){
+        series += i * j + ''
     }
     console.log(series);
 }
-console.log("Abdur Rahim");
-console.log('www.shatotacorporation.com');
--- apple.js
+++ apple.js
@@ -1,1 +1,9 @@
-userAnd.name && console.log(`Name is: ${userAnd.name}`);
+var Series
+
+for (var i = 1; i <= 10; i++){
+    series = i + ' = '
+    for (var j = 1; j < 10; j++) {
+        series += i * j + ' '
+    }
+    console.log(series);
+}
