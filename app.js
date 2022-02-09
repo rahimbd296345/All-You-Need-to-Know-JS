59d6b80db52c0262d74a68c91519db5cd6367df2
Abdur Rahim <www.shatotacorporation@gmail.com>
2022-02-09 21:10:10 +0600

    Today is end coding.

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-// Foor Loops
-for (var i = 0; i < 500; i++){
-    console.log('Rokon Razakar');
-}
-
-for (var i = 0; i < 5; i++){
-    console.log('I Love Bangladesh!');
-}
-
-for (var i = 0; i < 10; i++){
-    console.log(i+ 1) + ' Abdur Rahim';
-}
+// Today is end of coding...
