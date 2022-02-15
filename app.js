abb9ea285c808077822f3ae79ce07afc2527e02d
Abdur Rahim <www.shatotacorporation>
2022-02-15 08:16:39 +0600

    Okay

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // Today is end of coding...
+var Seriesone
+
+for(var i = 1; i <= 10; i++){
+    series = i + ' = '
+    for (var j = 1; j < 10; j++){
+        series += i * j + ' '
+    }
+    console.log(series);
+}
