62c720cb3f52e141675727918aadbcfd6c4e4546
Abdur Rahim <www.shatotacorporation@gmail.com>
2022-03-11 21:19:14 +0600

    Good Night

--- app.js
+++ app.js
@@ -1,35 +1,71 @@
-for (var i = 1; i <= 100; i++){
-    if (i % 2 == 1){
-        console.log(i);
-    }
+var a = 10;
+var b = 20;
+
+if (a > b) {
+  console.log("A is greater than B");
 }
 
-for (var i = 1; i <= 100; i++){
-    if(i % 2 == 0){
-        console.log(i);
-    }
+if (a < b) {
+  console.log("B is greater than A");
 }
 
-for (var i = 1; i <= 100; i++){
-    if(i % 2 == 0){
-        console.log(i);
-    }
+var a = 200;
+var b = 30;
+
+if (a > b) {
+  console.log("A is greater than B");
 }
 
-var sum = 0
-
-for (var i = 1; i <= 10; i++){
-    console.log(sum + ' + ' + i + ' = ' + (sum+i));
-    sum += i
+if (a < b) {
+  console.log("B is greater than A");
 }
 
-var sum = 0
-
-for ( var i = 1; i <= 10; i++){
-    console.log(sum + ' + ' + i + ' = ' + (sum+i));
-    sum += i
+var n = 16;
+if (n % 2 == 0) {
+  console.log(n + " is Even Number.");
 }
 
-for(let i = 0, j = 10; i <j, i ++; j--){
-    console.log(i + j);
+if (n % 2 == 1) {
+  console.log(n + " is Odd Number");
 }
+
+var p = 22;
+var q = 22;
+
+if (p > q) {
+  console.log("p is greater than q");
+}
+
+var p = 220;
+var q = 210;
+
+if (p > q) {
+  console.log("p is greater than q");
+} else if (p == q) {
+  console.log("p is equal to q");
+} else {
+  console.log("p is smaller than q");
+}
+
+var a = 1;
+var b = 2;
+
+if (a > b) {
+  console.log("A is greater than B");
+}
+
+if (a < b) {
+    console.log('B is greater than A');
+}
+
+var n = 51
+
+if (n % 2 == 0) {
+    console.log(n + ' is Even Number');
+}
+
+if (n % 2 == 1) {
+    console.log(n + ' is Odd Number');
+}
+
+
