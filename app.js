3a40c67dd982a6a67ffa1479b5aac7cd0454cc4b
Abdur Rahim <www.shatotacorporation@gmail.com>
2022-03-12 22:23:54 +0600

    Switch Statement..

--- app.js
+++ app.js
@@ -103,3 +103,40 @@ if (n == 0) {
   console.log(n + ' is Odd Number');
 }*/
 // Else If Statement End Hare..
+
+// Switch Statement Start Hare...
+
+var date = new Date()
+
+var today = date.getDay()
+
+switch (today) {
+  case 0:
+    console.log("Today is Sunday");
+    break;
+  case 1:
+    console.log("Today is Monday");
+    break;
+  case 2:
+    console.log("Today is Tuesday");
+    break;
+  case 3:
+    console.log("Today is Wednesday");
+    break;
+  case 4:
+    console.log("Today is Thursday");
+    break;
+  case 5:
+    console.log("Today is Friday");
+    break;
+  case 6:
+    console.log("Today is Satarday");
+    break;
+  
+
+  default:
+    console.log('Not a valid Number...');
+    break;
+}
+
+// Switch Statement End Hare..
