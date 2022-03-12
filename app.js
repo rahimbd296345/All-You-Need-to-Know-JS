22d5b00d1a4715622594ce716e52a0b94fe59e99
Abdur Rahim <www.shatotacorporation@gmail.com>
2022-03-12 22:14:28 +0600

    Today Learning Else If Statement..

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var a = 10;
+/*//var a = 10;
 var b = 20;
 
 if (a > b) {
@@ -67,5 +67,39 @@ if (n % 2 == 0) {
 if (n % 2 == 1) {
     console.log(n + ' is Odd Number');
 }
+ */
 
+// Else If Statement Start Hare...
+// var a = 20
+// var b = 20
 
+/*if (a > b) {
+  console.log('A is greater than B');
+} else {
+  console.log('B is greater than A');
+}*/
+
+/*if (a > b) {
+  console.log('A is greater than B');
+} else if (a < b) {
+  console.log('B is greater than A');
+} else {
+  console.log('They both are same..');
+} */
+
+// var n = 0
+
+/*if (n % 2 == 0) {
+  console.log(n + ' is Even Number');
+} else {
+  console.log(n + ' is Odd Number');
+}
+
+if (n == 0) {
+  console.log(n + ' is Zero');
+} else if (n % 2 == 0) {
+  console.log(n + ' is Even Number');
+} else {
+  console.log(n + ' is Odd Number');
+}*/
+// Else If Statement End Hare..
