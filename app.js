4189ac3ae55be17ecde7ab9ede22cc8df359c153
Abdur Rahim <www.shatotacorporation@gmail.com>
2022-03-13 09:26:20 +0600

    Learn With Free Code Camp..

--- app.js
+++ app.js
@@ -140,3 +140,109 @@ switch (today) {
 } */
 
 // Switch Statement End Hare..
+
+/*const age = 12
+
+if (age >= 18) {
+  console.log('Nick is an adult');
+} else {
+  console.log('Nick is a child');
+}*/
+
+/*const age = 22
+if (age < 18) {
+  console.log('Alice is under 18 years old.');
+} else if (age >= 18 && age <= 21) {
+  console.log('Alice is between the ages of 18 and 21');
+} else {
+  console.log('Alice is over 21 years old');
+}*/
+
+/*const pet = 'cat'
+
+if (pet === 'lizard') {
+  console.log('I own a lizard');
+} else if (pet === 'dog') {
+  console.log('I own a dog');
+} else if (pet === 'cat') {
+  console.log('I own a cat');
+} else if (pet === 'snake') {
+  console.log('I own a snake');
+} else if (pet === 'parrot') {
+  console.log('I own a parrot');
+} else {
+  console.log("I don't own a pet");
+}*/
+
+/* const pet = 'parrot'
+
+switch (pet) {
+  case "lizard":
+    console.log("I own a lizard");
+    break;
+  case "dog":
+    console.log("I own a dog");
+    break;
+  case "cat":
+    console.log("I own a cat");
+    break;
+  case "snake":
+    console.log("I own a snake");
+    break;
+  case "parrot":
+    console.log("I own a parrot");
+    break;
+  default:
+    console.log("I don't own a pet");
+    break
+}*/
+
+/* const age = 14
+const ownsCar = true
+
+if (age >= 16 && ownsCar) {
+  console.log('Jerry is old enough to drive and has his own car.');
+} else {
+  console.log('Jerry does not drive.');
+} */
+
+/* const boyfriendIsPaying = true
+const isSale = false
+
+if (boyfriendIsPaying || isSale) {
+  console.log('Jesse will go shopping.');
+} else {
+  console.log('Jess will not go shopping');
+}*/
+
+/* const boyfriendIsPaying = false
+const isSale = false
+
+if (boyfriendIsPaying || isSale) {
+  console.log('Jesse will go shopping');
+} else {
+  console.log('Jess will not go shopping');
+} */
+
+/* const boyfriendIsPaying = true
+const isSale = false
+
+if (!boyfriendIsPaying || isSale) {
+  console.log('Jess will go shopping');
+} else {
+  console.log('Jess will not go shopping');
+} */
+
+/* const age = 32
+const citizen = age >= 20 ? 'Can vote' : 'Cannot vote'
+console.log(citizen); */
+
+const age = 32
+let citizen
+
+if (age >= 18) {
+  citizen = 'Can vote'
+} else {
+  citizen = 'Cannot vote'
+}
+console.log(citizen);
--- chapter_three/if.js
+++ chapter_three/if.js
@@ -1,3 +1,116 @@
+// Example by w3schools.com
+
+if (new Date().getHours() < 18) {
+  document.getElementById("demo").innerHTML = "Good day!";
+}
+
+// https://www.freecodecamp.org
+
+const age = 18;
+
+if (age >= 18) {
+  console.log("Nick is an adult.");
+} else {
+  console.log("Nick is a child.");
+}
+
+const age = 18;
+
+if (age < 18) {
+  console.log("Alice is under 18 years old.");
+} else if (age >= 18 && age <= 21) {
+  console.log("Alice is between the ages of 18 and 21.");
+} else {
+  console.log("Alice is over 21 years old.");
+}
+
+const pet = "dog";
+
+if (pet === "lizard") {
+  console.log("I own a lizard");
+} else if (pet === "dog") {
+  console.log("I own a dog");
+} else if (pet === "cat") {
+  console.log("I own a cat");
+} else if (pet === "snake") {
+  console.log("I own a snake");
+} else if (pet === "parrot") {
+  console.log("I own a parrot");
+} else {
+  console.log("I don't own a pet");
+}
+
+const pet = "dog";
+ 
+switch (pet) {
+  case "lizard":
+    console.log("I own a lizard");
+    break;
+  case "dog":
+    console.log("I own a dog");
+    break;
+  case "cat":
+    console.log("I own a cat");
+    break;
+  case "snake":
+    console.log("I own a snake");
+    break;
+  case "parrot":
+    console.log("I own a parrot");
+    break;
+  default:
+    console.log("I don't own a pet");
+    break;
+}
+
+const age = 17;
+const ownsCar = true;
+
+if (age >= 16 && ownsCar) {
+  console.log("Jerry is old enough to drive and has his own car.");
+} else {
+  console.log("Jerry does not drive.");
+}
+
+
+/* const boyfriendIsPaying = true;
+const isSale = false;
+
+if (boyfriendIsPaying || isSale) {
+  console.log("Jesse will go shopping.");
+} else {
+  console.log("Jesse will not go shopping.");
+} */
+
+const boyfriendIsPaying = true;
+const isSale = false;
+
+if (!boyfriendIsPaying || isSale) {
+  console.log("Jesse will go shopping.");
+} else {
+  console.log("Jesse will not go shopping.");
+}
+
+/* const age = 32;
+const citizen = age >= 18 ? "Can vote" : "Cannot vote";
+console.log(citizen); */
+
+const age = 32;
+let citizen;
+
+if (age >= 18) {
+  citizen = "Can vote";
+} else {
+  citizen = "Cannot vote";
+}
+
+console.log(citizen);
+
+
+
+
+
+
 var a = 10
 var b = 20
 
