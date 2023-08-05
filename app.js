ae6f4771b7af83551bfba0afc89b6a36edcc0cd6
Abdur Rahim <99087922+rahimbd296345@users.noreply.github.com>
2023-08-05 21:40:21 +0600

    The Spread (...) Operator

---
+++ app.js
@@ -0,0 +1,10 @@
+const q1 = ['Jan', 'Feb', 'Mar'];
+const q2 = ['Apr', 'May', 'Jun'];
+const q3 = ['Jul', 'Aug', 'Sep'];
+const q4 = ['Oct', 'Nov', 'Dec'];
+
+const year = [...q1,...q2,...q3,...q4];
+
+console.log(year)
+
+
