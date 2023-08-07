8c3ca8475d6293c7915b11b14545e9efdc12c9eb
Abdur Rahim <99087922+rahimbd296345@users.noreply.github.com>
2023-08-07 10:29:14 +0600

    The Spread Operator

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-class Car {
-    constructor(name, year) {
-        this.name = name
-        this.year = year
+const myArr = [
+    {name: 'X00', price: 110},
+    {name: 'X01', price: 110},
+    {name: 'X02', price: 110},
+    {name: 'X03', price: 110},
+    {name: 'X04', price: 110},
+    {name: 'X05', price: 120},
+    {name: 'X06', price: 120},
+    {name: 'X07', price: 120},
+    {name: 'X08', price: 120},
+    {name: 'X09', price: 130},
+    {name: 'X10', price: 130},
+    {name: 'X11', price: 130},
+    {name: 'X12', price: 130},
+    {name: 'X13', price: 130},
+    {name: 'X14', price: 130},
+    {name: 'X15', price: 130},
+    {name: 'X16', price: 140},
+    {name: 'X17', price: 140},
+    {name: 'X18', price: 140},
+    {name: 'X19', price: 140},
+    {name: 'X20', price: 140},
+];
+
+myArr.sort(
+    p1, p2 => {
+        if (p1.price < p2.price) return -1;
+        if (p1.price > p2.price) return 1;
+        return 0;
     }
+);
+
+let txt = '';
+myArr.forEach(myFunction);
+
+function myFunction(value) {
+    txt += value.name + ' ' + value.price + '<br>'
 }
 
-const myCar1 = new Car('Ford', 2014)
-const myCar2 = new Car('Audi', 2019)
-
-console.log(myCar1.name)
-console.log(myCar2.name)
+console.log(txt);
